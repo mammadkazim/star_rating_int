f6ad6d82346c7eb517ab9c09a0c2c52b4a46dd3f
Kazim Mammadov <mammadkazim@gmail.com>
2023-10-06 02:19:19 -0400

    int task star rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'interview4';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+  stars: string[] = [];
+  constructor() {}
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      rate: new FormControl(null, Validators.required),
+    });
+  }
+  public onSubmit() {
+    let rate: number = this.myForm.value.rate;
+    let starsCount: number = Math.floor(rate);
+    this.stars = this.generateStars(starsCount, rate);
+    console.log(this.stars);
+    // this.myForm.reset();
+  }
+  public generateStars(count: number, rate: number) {
+    let starsArray: string[] = Array(count).fill('fa fa-star');
+    if (rate > count) starsArray.push('fa fa-star half-star');
+    return starsArray;
+  }
 }
